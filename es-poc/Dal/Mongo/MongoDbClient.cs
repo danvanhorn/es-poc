cb8e7952028a8a06f5b94211c68a510dad0dbcfc
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-02-04 15:59:16 -0800

    add mongo client

---
+++ es-poc/Dal/Mongo/MongoDbClient.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace es_poc.Dal.Mongo
+{
+    public static class MongoDbClient
+{
+        public static void initialize() {
+            var client = new MongoClient("http://localhost:8081");
+
+
+
+        }
+
+}
+}
