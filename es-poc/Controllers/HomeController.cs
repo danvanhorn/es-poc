70103fcb185a8cd55fdb06095f89d58658095829
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-02-20 10:14:08 -0800

    remove unneeded method

--- es-poc/Controllers/HomeController.cs
+++ es-poc/Controllers/HomeController.cs
@@ -72,14 +72,6 @@ namespace es_poc.Controllers
             return View("SearchDetailsViewModel", result.FirstOrDefault());
         }
 
-        public async Task<IActionResult> getBlobAsync()
-        {
-            AzureClient aClient = new AzureClient();
-
-            await aClient.getBlobDataAsync("1.jpg");
-            return View("Index");
-        }
-
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
